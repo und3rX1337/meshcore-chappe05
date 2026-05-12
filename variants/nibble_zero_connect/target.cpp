b74cfc6836189e2608ba94dbe5b8d616f48a3374
ViezeVingertjes <michael.overhorst@gmail.com>
2026-05-12 09:01:38 +0200

    Address review comments and sync with dev

--- variants/nibble_zero_connect/target.cpp
+++ variants/nibble_zero_connect/target.cpp
@@ -13,7 +13,7 @@ SensorManager sensors;
 
 #ifdef DISPLAY_CLASS
   DISPLAY_CLASS display;
-  MomentaryButton user_btn(PIN_USER_BTN, 1000, true, true);
+  MomentaryButton user_btn(PIN_USER_BTN, 1000, true);
 #endif
 
 #ifndef LORA_CR
@@ -25,21 +25,6 @@ bool radio_init() {
   rtc_clock.begin(Wire);
   
   return radio.std_init(&spi);
-}
-
-uint32_t radio_get_rng_seed() {
-  return radio.random(0x7FFFFFFF);
-}
-
-void radio_set_params(float freq, float bw, uint8_t sf, uint8_t cr) {
-  radio.setFrequency(freq);
-  radio.setSpreadingFactor(sf);
-  radio.setBandwidth(bw);
-  radio.setCodingRate(cr);
-}
-
-void radio_set_tx_power(uint8_t dbm) {
-  radio.setOutputPower(dbm);
 }
 
 mesh::LocalIdentity radio_new_identity() {
