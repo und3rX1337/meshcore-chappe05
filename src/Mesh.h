9325112de27c68cced37d651205bba6d367eb551
Sefinek <contact@sefinek.net>
2026-05-28 16:38:39 +0200

    fix: fix typos in source code comments

--- src/Mesh.h
+++ src/Mesh.h
@@ -212,12 +212,12 @@ public:
   void sendDirect(Packet* packet, const uint8_t* path, uint8_t path_len, uint32_t delay_millis=0);
 
   /**
-   * \brief  send a locally-generated Packet to just neigbor nodes (zero hops)
+   * \brief  send a locally-generated Packet to just neighbor nodes (zero hops)
   */
   void sendZeroHop(Packet* packet, uint32_t delay_millis=0);
 
   /**
-   * \brief  send a locally-generated Packet to just neigbor nodes (zero hops), with specific transort codes
+   * \brief  send a locally-generated Packet to just neighbor nodes (zero hops), with specific transport codes
    * \param transport_codes   array of 2 codes to attach to packet
   */
   void sendZeroHop(Packet* packet, uint16_t* transport_codes, uint32_t delay_millis=0);
