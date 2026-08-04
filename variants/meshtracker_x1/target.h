36cce23304030a8d33397606ab3b3b4b45272ef6
Hacuchino-hash <246103064+Hacuchino-hash@users.noreply.github.com>
2026-08-04 12:59:16 -0500

    Add support for Seeed SenseCAP MeshTracker X1 (LR2021)

---
+++ variants/meshtracker_x1/target.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#define RADIOLIB_STATIC_ONLY 1
+#include <RadioLib.h>
+#include <helpers/radiolib/RadioLibWrappers.h>
+#include "MeshTrackerX1Board.h"
+#include <helpers/radiolib/CustomLR2021Wrapper.h>
+#include <helpers/ArduinoHelpers.h>
+#include <helpers/SensorManager.h>
+#include <helpers/sensors/LocationProvider.h>
+#ifdef DISPLAY_CLASS
+  #include "NullDisplayDriver.h"
+#endif
+
+class MeshTrackerX1SensorManager: public SensorManager {
+  bool gps_active = false;
+  LocationProvider * _nmea;
+
+  void start_gps();
+  void sleep_gps();
+  void stop_gps();
+public:
+  MeshTrackerX1SensorManager(LocationProvider &nmea): _nmea(&nmea) { }
+  bool begin() override;
+  bool querySensors(uint8_t requester_permissions, CayenneLPP& telemetry) override;
+  void loop() override;
+  int getNumSettings() const override;
+  const char* getSettingName(int i) const override;
+  const char* getSettingValue(int i) const override;
+  bool setSettingValue(const char* name, const char* value) override;
+  LocationProvider* getLocationProvider() { return _nmea; }
+};
+
+#ifdef DISPLAY_CLASS
+  extern NullDisplayDriver display;
+#endif
+
+extern MeshTrackerX1Board board;
+extern WRAPPER_CLASS radio_driver;
+extern VolatileRTCClock rtc_clock;
+extern MeshTrackerX1SensorManager sensors;
+
+bool radio_init();
+mesh::LocalIdentity radio_new_identity();
